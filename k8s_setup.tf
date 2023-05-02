a726e56381eeb708778c57a9526d9e91afe5a5fa
Mahi Singh <107246099+Mahigurjarr@users.noreply.github.com>
2023-05-02 17:21:47 +0530

    Update k8s_setup.tf

--- k8s_setup.tf
+++ k8s_setup.tf
@@ -7,19 +7,26 @@
     required_version = ">= 0.13"
   }
   provider "scaleway" {
-    access_key      = "SCW3WDMRCB8ND02Q7HDV"
-    secret_key      = "a4d86698-faf7-4afb-9dee-91925fcea703"
-    project_id	    = "33ebcbf6-7407-4e6f-8d64-b2bee2bf8a41"
+#     access_key      = "SCW3WDMRCB8ND02Q7HDV"
+#     secret_key      = "a4d86698-faf7-4afb-9dee-91925fcea703"
+#     project_id	    = "33ebcbf6-7407-4e6f-8d64-b2bee2bf8a41"
     zone            = "fr-par-1"
     region          = "fr-par"
   }
+   cloud {
+       organization = "REPLACE_ME"
 
-  resource "scaleway_k8s_cluster" "k8s-mafao-dev" {
-  name    = "k8s-mafao-dev"
-  version = "1.24.3"
-  cni     = "cilium"
-  delete_additional_resources = false
-}
+       workspaces {
+         name = "learn-terraform-github-actions"
+       }
+     }
+   }
+   resource "scaleway_k8s_cluster" "k8s-mafao-dev" {
+   name    = "k8s-mafao-dev"
+   version = "1.24.3"
+   cni     = "cilium"
+   delete_additional_resources = false
+ }
 
 resource "scaleway_k8s_pool" "mafao_dev_keycloak" {
   cluster_id = scaleway_k8s_cluster.k8s-mafao-dev.id
