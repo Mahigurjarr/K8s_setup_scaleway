23860566e14308a6a4689a89bca89145003d2cc1
Mahigurjarr <mahipalgurjar99@gmail.com>
2023-05-02 17:54:15 +0530

    remove crendentials

--- k8s_setup.tf
+++ k8s_setup.tf
@@ -7,9 +7,6 @@
     required_version = ">= 0.13"
   }
   provider "scaleway" {
-#     access_key      = "SCW3WDMRCB8ND02Q7HDV"
-#     secret_key      = "a4d86698-faf7-4afb-9dee-91925fcea703"
-#     project_id	    = "33ebcbf6-7407-4e6f-8d64-b2bee2bf8a41"
     zone            = "fr-par-1"
     region          = "fr-par"
   }
