197dfd11cf0f36d176e45067978acde2912db553
Mahi Singh <107246099+Mahigurjarr@users.noreply.github.com>
2023-05-17 01:10:51 +0530

    Update k8s_setup.tf

--- k8s_setup.tf
+++ k8s_setup.tf
@@ -11,11 +11,12 @@
     region          = "fr-par"
   }
 resource "scaleway_k8s_cluster" "k8s-mafao-dev" {
-   name    = "k8s-mafao-dev"
-   version = "1.24.3"
-   cni     = "cilium"
-   delete_additional_resources = false
-  }
+ project_id = "33ebcbf6-7407-4e6f-8d64-b2bee2bf8a41"
+ name    = "k8s-mafao-dev"
+ version = "1.24.3"
+ cni     = "cilium"
+ delete_additional_resources = false
+ }
 
   resource "scaleway_k8s_pool" "mafao_dev_keycloak" {
   cluster_id = scaleway_k8s_cluster.k8s-mafao-dev.id
