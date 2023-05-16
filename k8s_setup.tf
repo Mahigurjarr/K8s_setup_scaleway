0e05f5d6fda81730480232ba93b409759baaa051
Mahi Singh <107246099+Mahigurjarr@users.noreply.github.com>
2023-05-17 01:08:12 +0530

    Update k8s_setup.tf

--- k8s_setup.tf
+++ k8s_setup.tf
@@ -10,22 +10,14 @@
     zone            = "fr-par-1"
     region          = "fr-par"
   }
-  cloud {
-       organization = "Alyssum"
-
-       workspaces {
-         name = "terraform_practice"
-      }
-  }
-   
-  resource "scaleway_k8s_cluster" "k8s-mafao-dev" {
+resource "scaleway_k8s_cluster" "k8s-mafao-dev" {
    name    = "k8s-mafao-dev"
    version = "1.24.3"
    cni     = "cilium"
    delete_additional_resources = false
   }
 
-resource "scaleway_k8s_pool" "mafao_dev_keycloak" {
+  resource "scaleway_k8s_pool" "mafao_dev_keycloak" {
   cluster_id = scaleway_k8s_cluster.k8s-mafao-dev.id
   name       = "mafao_dev_keycloak"
   node_type  = "PLAY2-NANO"
@@ -34,4 +26,5 @@ resource "scaleway_k8s_pool" "mafao_dev_keycloak" {
   size = 1
   autoscaling = true
   autohealing = true
-}
+  }
+
